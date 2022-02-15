44a4d1b89a28b1fd581040a9a6d6cf3fcb822d4b
Pedro Delicoli <pedrodelicoli@hotmail.com>
2022-02-15 17:55:50 -0300

    Confira middleware de erro

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const router = require('../routes')
+const error = require('../middlewares/error')
 
 const app = express();
 app.use(express.json());
+
+app.use(router);
+app.use(error);
 
 module.exports = app;
